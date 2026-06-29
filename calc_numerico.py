397fd3d4b0893f60d1b92e4896b5db7d6e49667e
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2026-06-29 20:48:42 -0300

    feat(interpolacao): implementação de função para gerar gráfico dos pontos interpolados

--- calc_numerico.py
+++ calc_numerico.py
@@ -1,7 +1,3 @@
-import interpolacao
 import normalizacao
 
 tempos, temperaturas = normalizacao.carregar_dados("./dados_resfriamento.csv")
-
-valor = interpolacao.lagrange(tempos, temperaturas, 5)
-print(valor)
--- interpolacao.py
+++ interpolacao.py
@@ -1,4 +1,5 @@
 from math import prod as produtorio
+from numpy import linspace
 somatorio = sum
 
 def lagrange(x, y, ponto):
@@ -12,3 +13,14 @@ def lagrange(x, y, ponto):
         )
         for i in range(len(x))
     )
+
+def gerar_curva(x, y, quantidade=500):
+
+    eixo_x = linspace(min(x), max(x), quantidade)
+
+    eixo_y = [
+        lagrange(x, y, ponto)
+        for ponto in eixo_x
+    ]
+
+    return eixo_x, eixo_y
